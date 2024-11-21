a4fc7e6961f78ba33f0f00ccf018d6258cf60690
Eswar090906 <seswar2006@gmail.com>
2024-11-21 12:56:28 +0530

    success

---
+++ bankloan/loan_detail/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib import admin
+
+class BankLoan(models.Model):
+    loan_id=models.IntegerField(primary_key=True)
+    loan_type=models.CharField(max_length=30)
+    loan_amt=models.FloatField()
+    cust_acno=models.IntegerField()
+    cust_name=models.CharField(max_length=50)
+    
+class BankLoanAdmin(admin.ModelAdmin):
+   list_display = ('loan_id','loan_type','loan_amt','cust_acno','cust_name')
